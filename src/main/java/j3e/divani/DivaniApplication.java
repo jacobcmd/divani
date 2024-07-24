f13a52f233ca517c635a643335b1eff44bd87643
Jacob Mendoza <jacob.mendoza.hdz@gmail.com>
2024-07-24 15:18:59 -0600

    first commit

---
+++ src/main/java/j3e/divani/DivaniApplication.java
@@ -0,0 +1,13 @@
+package j3e.divani;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivaniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivaniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/j3e/divani/DivaniApplicationTests.java
@@ -0,0 +1,13 @@
+package j3e.divani;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivaniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
